adc1becddfa758ce9e9db791fad672d4a0cfe76b
PyRadiolarus <60543948+PyRadiolarus@users.noreply.github.com>
2021-07-04 22:58:25 +0900

    fix ABC208

--- AtCoderBeginnerContest/208_C.py
+++ AtCoderBeginnerContest/208_C.py
@@ -5,11 +5,11 @@ N, K = map(int, stdin.readline().split())
 a = [int(i) for i in stdin.readline().split()]
 a_ = sorted(a,reverse=True)
 b, k = divmod(K, N)
-k = k * (-1)
+k = k * -1
 for i in a:
     if k == 0:
         print(b)
     elif i <= a_[k] and N != 1:
-        print(b+1)
+        print(b + 1)
     else:
         print(b)
